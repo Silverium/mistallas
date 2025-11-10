db799df64749c4c260e2387bc09d93e22c1ba92a
Soldeplata <soldeplatadeveloper@gmail.com>
2025-11-10 11:28:09 +0100

    fix: update welcome message and descriptions for better user guidance

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,34 +1,16 @@
 <template>
   <div class="flex flex-col gap-4">
     <p class="font-medium">
-      ¡Bienvenido!
+      ¿Te acuerdas de la talla y modelo de pantalones que necesitas de esa marca que cambia el tallaje incluso cuando se elije otro color del pantalón?
     </p>
     <p>
-      A <a
-        href="https://nuxt.com"
-        target="_blank"
-        class="text-primary"
-        rel="noopener"
-      >Nuxt</a> demo hosted on <a
-        href="https://pages.cloudflare.com"
-        target="_blank"
-        rel="noopener"
-        class="text-primary"
-      >Cloudflare Pages</a> with server-side rendering on the edge and using <NuxtLink
-        href="https://developers.cloudflare.com/d1/"
-        target="_blank"
-        rel="noopener"
-        class="text-primary"
-      >
-        Cloudflare D1 database
-      </NuxtLink>.
+      ¿O de la talla de zapatos que usas en esa tienda online extranjera que siempre talla diferente?
     </p>
     <p>
-      It is made using <a
-        href="https://hub.nuxt.com"
-        class="text-primary"
-      >NuxtHub</a> for a zero-config development & deployment experience on Cloudflare.
+      Esta es una aplicación sencilla para gestionar tus tallas de ropa, calzado y accesorios.
+      Inicia sesión con tu cuenta de GitHub para comenzar a guardar y administrar tus tallas de manera segura.
     </p>
+
     <USeparator />
     <p class="text-sm text-muted italic">
       No se almacena ninguna información personal de tu cuenta de GitHub en la base de datos.<br>
