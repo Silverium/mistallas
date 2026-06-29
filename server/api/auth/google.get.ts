471d5b8b92228e9311d18283f66c104fe50e893c
Soldeplata <soldeplatadeveloper@gmail.com>
2026-06-29 09:00:50 +0200

    feat: redirect to purchases instead of todos after successful OAuth login

--- server/api/auth/google.get.ts
+++ server/api/auth/google.get.ts
@@ -2,6 +2,6 @@ export default defineOAuthGoogleEventHandler({
   async onSuccess(event, { user }) {
     console.log(`%cuser`, 'background-color: lime;', user)
     await setUserSession(event, { user: { ...user, id: user.email } })
-    return sendRedirect(event, '/todos')
+    return sendRedirect(event, '/purchases')
   }
 })
