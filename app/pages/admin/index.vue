92f119d3b39e311e7e4234396528664458a7109b
Soldeplata <soldeplatadeveloper@gmail.com>
2026-07-05 12:35:10 +0200

    feat: add admin page with redirection to user management

---
+++ app/pages/admin/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: 'admin'
+})
+
+await navigateTo('/admin/users', { replace: true })
+</script>
+
+<template>
+  <div class="sr-only">
+    Redirigiendo...
+  </div>
+</template>
